e3a0b1d5577539a2925cd5bda24e60944f740419
leewooyup <zjadlspun1114@naver.com>
2022-12-05 11:31:46 +0900

    세팅

---
+++ fileUpload1205/src/main/java/com/ll/exam/fileUpload1205/FileUpload1205Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.fileUpload1205;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUpload1205Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUpload1205Application.class, args);
+	}
+
+}
---
+++ fileUpload1205/src/test/java/com/ll/exam/fileUpload1205/FileUpload1205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.fileUpload1205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUpload1205ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
