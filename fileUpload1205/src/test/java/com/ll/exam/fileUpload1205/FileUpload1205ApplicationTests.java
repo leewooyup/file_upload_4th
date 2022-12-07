eee4cf7c7bbbfc82092cbb08226199f46f5a1fcf
leewooyup <zjadlspun1114@naver.com>
2022-12-07 14:43:41 +0900

    MockMvc 테스트, GET / 요청의 응답 본문에 안녕이 포함

--- fileUpload1205/src/test/java/com/ll/exam/fileUpload1205/FileUpload1205ApplicationTests.java
+++ fileUpload1205/src/test/java/com/ll/exam/fileUpload1205/FileUpload1205ApplicationTests.java
@@ -1,13 +1,43 @@
 package com.ll.exam.fileUpload1205;
 
+import com.ll.exam.fileUpload1205.app.home.controller.HomeController;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import javax.transaction.Transactional;
+
+
+import static org.hamcrest.Matchers.containsString;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 class FileUpload1205ApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("메인화면에서는 안녕이 나와야 한다.")
+	void t1() throws Exception {
+		ResultActions resultActions = mvc
+				.perform(get("/"))
+				.andDo(print());
+
+		resultActions
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(HomeController.class))
+				.andExpect(handler().methodName("main"))
+				.andExpect(content().string(containsString("안녕")));
 	}
 
 }
