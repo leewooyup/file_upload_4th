59ae4ddb7834463e9033c1af96d28112a7707e56
leewooyup <zjadlspun1114@naver.com>
2022-12-19 15:01:38 +0900

    User객체 대신 MemberContext를 이용

--- fileUpload1205/src/main/java/com/ll/exam/fileUpload1205/app/member/service/MemberService.java
+++ fileUpload1205/src/main/java/com/ll/exam/fileUpload1205/app/member/service/MemberService.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService implements UserDetailsService {
+public class MemberService {
     @Value("${custom.genFileDirPath}")
     private String genFileDirPath;
 
@@ -78,16 +78,6 @@ public class MemberService implements UserDetailsService {
 
     public Member getMemberById(Long id) {
         return memberRepository.findById(id).orElse(null);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByUsername(username).get();
-
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("member"));
-
-        return new User(member.getUsername(), member.getPassword(), authorities);
     }
 
     public long count() {
