a00df28c6f710e8b8ffe9730b3e8da34703b1415
leewooyup <zjadlspun1114@naver.com>
2022-12-19 11:45:03 +0900

    파일의 확장자 유지, 저장폴더 날짜로 세분화

--- fileUpload1205/src/main/java/com/ll/exam/fileUpload1205/app/member/service/MemberService.java
+++ fileUpload1205/src/main/java/com/ll/exam/fileUpload1205/app/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.ll.exam.fileUpload1205.app.member.service;
 
 import com.ll.exam.fileUpload1205.app.member.entity.Member;
 import com.ll.exam.fileUpload1205.app.member.repository.MemberRepository;
+import com.ll.exam.fileUpload1205.app.util.Util;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -33,8 +34,9 @@ public class MemberService implements UserDetailsService {
     }
 
     public Member join(String username, String password, String email, MultipartFile profileImg) {
-        String profileImgDirName = "member";
-        String fileName = UUID.randomUUID().toString() + ".png";
+        String profileImgDirName = "member/" + Util.date.getCurrentDateFormatted("yyyy_MM_dd");
+        String ext = Util.file.getExt(profileImg.getOriginalFilename());
+        String fileName = UUID.randomUUID().toString() + "." + ext;
         String profileImgDirPath = genFileDirPath + "/" + profileImgDirName;
         String profileImgFilePath = profileImgDirPath + "/" + fileName;
 
