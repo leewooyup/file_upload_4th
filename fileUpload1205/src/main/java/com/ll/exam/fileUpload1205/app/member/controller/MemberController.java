e4f2c6e1287e05b8f6bf106ea728c037c1909bcb
leewooyup <zjadlspun1114@naver.com>
2022-12-19 17:24:42 +0900

    이미지를 경유해서 보여줌

--- fileUpload1205/src/main/java/com/ll/exam/fileUpload1205/app/member/controller/MemberController.java
+++ fileUpload1205/src/main/java/com/ll/exam/fileUpload1205/app/member/controller/MemberController.java
@@ -9,10 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
@@ -63,5 +60,10 @@ public class MemberController {
     @GetMapping("/profile")
     public String showProfile() {
         return "member/profile";
+    }
+
+    @GetMapping("/profile/img/{id}")
+    public String showProfileImg(@PathVariable Long id) {
+        return "redirect:" + memberService.getMemberById(id).getProfileImgUrl();
     }
 }
