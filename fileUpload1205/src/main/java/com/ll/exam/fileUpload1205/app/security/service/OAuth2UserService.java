925ec5dbf37622c3181aab73430e387ccbf5558b
leewooyup <zjadlspun1114@naver.com>
2022-12-24 19:11:23 +0900

    로그 디버그 활성화, 카카오톡 프로필 이미지 받기

--- fileUpload1205/src/main/java/com/ll/exam/fileUpload1205/app/security/service/OAuth2UserService.java
+++ fileUpload1205/src/main/java/com/ll/exam/fileUpload1205/app/security/service/OAuth2UserService.java
@@ -7,6 +7,7 @@ import com.ll.exam.fileUpload1205.app.member.repository.MemberRepository;
 import com.ll.exam.fileUpload1205.app.member.service.MemberService;
 import com.ll.exam.fileUpload1205.app.security.dto.MemberContext;
 import com.ll.exam.fileUpload1205.app.security.exception.OAuthTypeMatchNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -24,6 +25,7 @@ import java.util.*;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class OAuth2UserService extends DefaultOAuth2UserService {
     @Autowired
     private MemberRepository memberRepository;
@@ -52,9 +54,11 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
         if (isNew(oauthType, oauthId)) {
             switch (oauthType) {
                 case "KAKAO" -> {
+                    log.debug("attributes: " + attributes);
                     Map attributesProperties = (Map) attributes.get("properties");
                     Map attributesKakaoAcount = (Map) attributes.get("kakao_account");
                     String nickname = (String) attributesProperties.get("nickname");
+                    String profile_image = (String) attributesProperties.get("profile_image");
                     String email = "%s@kakao.com".formatted(oauthId);
                     String username = "KAKAO_%s".formatted(oauthId);
 
@@ -69,7 +73,7 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
                             .build();
 
                     memberRepository.save(member);
-                    memberService.setProfileImgByUrl(member, "https://picsum.photos/200/300");
+                    memberService.setProfileImgByUrl(member, profile_image);
                 }
             }
         } else {
